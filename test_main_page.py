fd802d7cee075b8751fa6af442182cd4112ce064
EvgenyDantsov <adriano-sport@mail.ru>
2025-07-28 19:21:24 +0300

    updated test_main_page, deleted webdriverwait

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,3 @@
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
@@ -8,11 +6,7 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()
-    WebDriverWait(browser, 10).until(
-        EC.url_contains("login"),
-        "URL does not contain 'login'"
-    )
-    login_page = LoginPage(browser, browser.current_url)  # Инициализируем LoginPage
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()  # Проверяем все элементы на странице логина
 
 def test_guest_should_see_login_link(browser):
